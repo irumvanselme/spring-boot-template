f6e47e5c0848370ae51daaa4558c771c4ba06486
anselme <andesanselme@gmail.com>
2021-10-31 18:38:42 +0200

    Make the body in the request body

--- src/main/java/com/okava/pay/controllers/AuthenticationController.java
+++ src/main/java/com/okava/pay/controllers/AuthenticationController.java
@@ -5,10 +5,7 @@ import com.okava.pay.utils.Formatter;
 import com.okava.pay.utils.dtos.RegisterDTO;
 import com.okava.pay.utils.payload.ApiResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -28,7 +25,7 @@ public class AuthenticationController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<ApiResponse> register(@Valid RegisterDTO dto){
+    public ResponseEntity<ApiResponse> register(@Valid @RequestBody RegisterDTO dto){
         return Formatter.ok(userService.create(dto));
     }
 
